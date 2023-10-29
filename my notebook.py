69a529c9e4ca48fc81e534f7b5096e886b9d7dd8
piyushgauti <143255888+piyushgauti@users.noreply.github.com>
2023-10-29 16:45:40 +0530

    Add files via upload

---
+++ my notebook.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# my notebook
+
+# this is introductory sentence for my notebook
+# 
+
+# some of the coommonly used libraries by data scientist include:
+# 
+# TensorFlow.
+# NumPy.
+# SciPy.
+# Pandas.
+# Matplotlib.
+# Keras.
+# PyTorch.
+# Scrapy.
+# 
+
+# some popular languages that data scientist used are:
+# python.
+# R.
+# SQL.
+# Java.
+# Julia.
+# Scala.
+
+# | Tool Name     | Description                       |
+# | ------------- | --------------------------------- |
+# | Python        | General-purpose programming language used in data science. |
+# | Jupyter Notebook | Interactive environment for data analysis and visualization. |
+# | Pandas        | Data manipulation and analysis library. |
+# | NumPy         | Library for numerical computing in Python. |
+# | Matplotlib    | Library for creating static, animated, and interactive visualizations. |
+# | Seaborn       | Data visualization library built on top of Matplotlib. |
+# | Scikit-learn  | Machine learning library for Python. |
+# | TensorFlow    | Open-source machine learning framework developed by Google. |
+# | PyTorch       | Deep learning framework for machine learning and AI. |
+# | R             | Programming language for statistical analysis and data visualization. |
+# | SQL           | Language for managing and querying relational databases. |
+# 
+
+# | Tool Name     | Description                       |
+# | ------------- | --------------------------------- |
+# | Python        | General-purpose programming language used in data science. |
+# | Jupyter Notebook | Interactive environment for data analysis and visualization. |
+# | Anaconda        | Data manipulation and analysis library. |
+# | R studio        | Library for numerical computing in Python. |
+# | Matplotlib    | Library for creating static, animated, and interactive visualizations. |
+# | Spyder       | Data visualization library built on top of Matplotlib. |
+# | Scikit-learn  | Machine learning library for Python. |
+# | TensorFlow    | Open-source machine learning framework developed by Google. |
+# | Zeppelin       | Deep learning framework for machine learning and AI. |
+# | R             | Programming language for statistical analysis and data visualization. |
+# | SQL           | Language for managing and querying relational databases. |
+# 
+
+# Arithmetic expressions are fundamental mathematical representations that involve operations such as addition, subtraction, multiplication, and division. They are used to perform calculations and are a core concept in mathematics and computer programming.
+# 
+
+# In[2]:
+
+
+(3*4)+5 #multiply and add integer
+
+
+# In[3]:
+
+
+# Conversion from minutes to hours
+minutes = 120  # Replace with the number of minutes you want to convert
+hours = minutes / 60
+
+# Display the result
+hours
+
+
+# Introduction to Fruits
+# 
+# Unordered List of Fruits:
+# - Apples
+# - Bananas
+# - Oranges
+# - Strawberries
+# - Grapes
+# 
+
+# **Author:** [piyush]
+# 
